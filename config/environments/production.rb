deed8c6c244b38ad6ff980ca15386e5a78c0d8ba
Selvaganapathy <k.selvaganapathy@yahoo.com>
2018-08-24 23:00:29 +0530

    Add users account with Devise Gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for Devise.
+  config.action_mailer.default_url_options = { host: 'www.yesbrands.com', port: 3000 }
 end
