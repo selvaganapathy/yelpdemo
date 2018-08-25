67bfffe6279fb5f7658467f883b042fbedcf2616
Selvaganapathy <k.selvaganapathy@yahoo.com>
2018-08-25 12:38:59 +0530

    associated the reviews to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_054104) do
+ActiveRecord::Schema.define(version: 2018_08_25_064224) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_08_25_054104) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
