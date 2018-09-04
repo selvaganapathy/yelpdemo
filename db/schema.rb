b2c897bc063b26e75b641892a9189e9c1dc08d0f
Selvaganapathy <k.selvaganapathy@yahoo.com>
2018-09-04 17:40:03 +0530

    add fields to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_064224) do
+ActiveRecord::Schema.define(version: 2018_09_04_114327) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 2018_08_25_064224) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
