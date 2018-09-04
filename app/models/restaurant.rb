615b1f33f23eaaabee685575fa521ce9a729f346
Selvaganapathy <k.selvaganapathy@yahoo.com>
2018-09-04 18:52:29 +0530

    Add Data Validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,11 @@ class Restaurant < ApplicationRecord
 	mount_uploader :image, ImageUploader
 
 	has_many :reviews
+	validates :name, :address, :phone, :weblink, :image, presence: true
+	validates :phone, format: {with: /\A\(\+\d{2}\) \d{10}\z/, 
+		message: "must be in the format (+91) 9876543210"
+	}
+	validates :phone, format: {with: /\Ahttps?\:\/\/.*\z/, 
+		message: "must start with http:// or https://"
+	}
 end
